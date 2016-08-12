2d1ba5ad4f8f73226bd9cd6965d0bb41b75dd51f
Tim Younger <tim@webpt.com>
2016-08-11 17:37:03 -0700

    replace static class method with a namespaced function. usage will look a bit simpler.

--- src/NoHtml/NoHtml.php
+++ src/NoHtml/NoHtml.php
@@ -2,16 +2,13 @@
 
 namespace NoHtml;
 
-class NoHtml
+/**
+ * @source https://paragonie.com/blog/2015/06/preventing-xss-vulnerabilities-in-php-everything-you-need-know
+ * @param $input
+ * @param string $encoding
+ * @return string
+ */
+function noHtml($input, $encoding = 'UTF-8')
 {
-    /**
-     * @source https://paragonie.com/blog/2015/06/preventing-xss-vulnerabilities-in-php-everything-you-need-know
-     * @param $input
-     * @param string $encoding
-     * @return string
-     */
-    public static function filter($input, $encoding = 'UTF-8')
-    {
-        return htmlentities($input, ENT_QUOTES | ENT_HTML5, $encoding);
-    }
+    return htmlentities($input, ENT_QUOTES | ENT_HTML5, $encoding);
 }
